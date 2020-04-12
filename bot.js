2afa669447412ebf10bdce577c70e591c3e4979f
Courtney Banister <s3794616@student.rmit.edu.au>
2020-04-12 19:47:11 +1000

    Update bot.js
    Starting code for calculation. Still needs some work.

--- bot.js
+++ bot.js
@@ -15,9 +15,18 @@ client.on('message', msg => {
   if (msg.content === `${prefix}status`) {
     msg.channel.send('Online! :robot:');
   }
-
-  // The command 'nm calc' followed by calculation causes the bot
-  // to return result of calculation.
+// The command 'nm' followed by a calculation returns an answer
+// to the user.
+// calculation cannot have spaces. Only one space after 'nm'
+  if (msg.content.includes(`${prefix}`) && msg.content.includes('+' || '-'
+      || '*' || '^')) {
+    var content = msg.content
+    var parts = content.split(' ', 2)
+    var problem = parts[1]
+    var answer = eval(problem)
+    msg.channel.send(answer);
+    }
+  }
 
 });
 
