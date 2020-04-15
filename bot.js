d34b7f434c5857e02e1ff6e46e00307711a272ab
Courtney Banister <s3794616@student.rmit.edu.au>
2020-04-16 09:22:04 +1000

    Update bot.js
    Added fail state message for spaces between characters

--- bot.js
+++ bot.js
@@ -19,15 +19,22 @@ client.on('message', msg => {
 // to the user.
 // calculation cannot have spaces. Only one space after 'nm'
 // Example 'nm 2+2' will return '4'.
-  if (msg.content.includes(`${prefix}`) && msg.content.includes('+' || '-'
+  var content = msg.content
+  var spaceCount = (content.split(' ').length - 1)
+  if (spaceCount > 1) {
+  msg.channel.send('Calculation not possible.\n' +
+                  'Please re-enter calculation.\n' +
+                  'Make sure calculation is formatted ' +
+                  'properly.\n' + 'Example: nm 2+2')
+  }
+  else if (msg.content.includes(`${prefix}`) && msg.content.includes('+' || '-'
       || '*' || '^')) {
-    var content = msg.content
     var parts = content.split(' ', 2)
     var problem = parts[1]
     var answer = eval(problem)
     msg.channel.send(answer);
     }
-  }
+}
 
 });
 
