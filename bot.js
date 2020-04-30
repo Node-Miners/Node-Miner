78e6a0dc99ca68ac62853011c05a9135c822fd29
factoryhub <39697596+factoryhub@users.noreply.github.com>
2020-04-30 22:04:44 +1000

    Revert "Calculator"

--- bot.js
+++ bot.js
@@ -16,18 +16,6 @@ client.on('message', msg => {
   if (msg.content === `${prefix}status`) {
     msg.channel.send('Online! :robot:');
   }
-// The command 'nm' followed by a calculation returns an answer
-// to the user.
-// Use of regex searches for individual symbols to determine whether it is
-// a maths expresion.
-let tempMsg = msg.content
-let characters = /[\+\*-\/]/;
-  if (tempMsg.startsWith('nm') && characters.exec(tempMsg) !== null) {
-      let parts = tempMsg.split(' ', 2)
-      let problem = parts[1]
-      let answer = eval(problem)
-      msg.channel.send(answer);
-    }
 
 });
 
