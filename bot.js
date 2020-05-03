33d359496c19d90ab16669682634fa5f18219659
Robertg <robert.gherlenda@gmail.com>
2020-05-03 23:27:55 +1000

    updating message

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ client.on('message', msg => {
   }
 
         if (msg.content ===  `${prefix}poma 20mins`) {
-            msg.channel.send(`${member.mention} 20 min timer set`);
+            msg.channel.send("""{member.mention} 20 min timer set""");
             
             setTimeout(function(){
               msg.channel.send("15 mins remaining")}, 300000);
