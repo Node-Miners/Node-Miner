1339c24fc63197e20685c93c104942e124c0eb53
factoryhub <39697596+factoryhub@users.noreply.github.com>
2020-05-06 18:53:53 +1000

    removing old calc code

--- bot.js
+++ bot.js
@@ -17,18 +17,8 @@ client.on('message', msg => {
     msg.channel.send('Online! :robot:');
   }
  
-// The command 'nm' followed by a calculation returns an answer
 // The command 'nm calc' followed by a calculation returns an answer
 // to the user.
-// Use of regex searches for individual symbols to determine whether it is
-// a maths expresion.
-let tempMsg = msg.content
-let characters = /[\+\*-\/]/;
-  if (tempMsg.startsWith('nm') && characters.exec(tempMsg) !== null) {
-      let parts = tempMsg.split(' ', 2)
-      let problem = parts[1]
-  }
-  
   if (msg.content.startsWith(`${prefix}calc`)) {
       let parts = msg.content.split(' ', 3)
       let problem = parts[2]
