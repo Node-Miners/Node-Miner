32531896853dc658c23cd850b5dbfeb1193f69bf
LynetteS8 <58105968+LynetteS8@users.noreply.github.com>
2020-05-07 14:34:53 +0930

    Add in basic timer feature, does not countdown

--- bot.js
+++ bot.js
@@ -11,12 +11,14 @@ client.once('ready', () => {
 
 // Event listener when a user sends a message in the chat
 client.on('message', msg => {
+//*LK* need to ender code here to convert input to uppercase or lowercase so
+//input does not need to be case sensitive
 
   // The command 'nm status' causes the bot to say 'Online!'
   if (msg.content === `${prefix}status`) {
     msg.channel.send('Online! :robot:');
   }
- 
+
 // The command 'nm calc' followed by a calculation returns an answer
 // to the user.
   if (msg.content.startsWith(`${prefix}calc`)) {
@@ -25,6 +27,35 @@ client.on('message', msg => {
       let answer = eval(problem)
       msg.channel.send(answer);
     }
+
+    let minutesToTime = '';
+        function delayText(milliseconds){
+        }
+            if (msg.content.startsWith('${prefix} TIMER')){
+                // to split the input and get the varible to use in calculations
+                let timerParts = message.content.split(' ',3)
+                minutesToTime = timerParts[2]
+                msg.reply(minutesToTime)
+                msg.channel.send('mins timer set');//should be on line above??
+
+                //This converts overall timer time to milliseconds
+                msForTimer = 60000*minutesToTime;
+
+                    //this is to make the timer countdown - not working yet
+                    while (minutesToTime > 0) { //this does not yet countdown
+                            minsRemaining = (minutesToTime - 1)*60000
+                        //setTimeout(function(){
+                         //msg.reply(minsRemaining)}, (minsRemaining));
+                            minutesToTime = minutesToTime - 1;
+                    }
+                    //message displays after timer finished
+                    if (minutesToTime === 0 )
+                        setTimeout(function(){
+                        msg.reply('**Timer ended**')}, (msForTimer));
+
+
+                }
+
 });
 
 
