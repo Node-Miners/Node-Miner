77cc62e2e8ea3294931c4a35a0fc351b521f89be
AndrewPritch <51275062+AndrewPritch@users.noreply.github.com>
2020-05-13 20:25:37 +1000

    Updated code that works
    Uploaded code that actually works

--- reminder2/reminder2.js
+++ reminder2/reminder2.js
@@ -1,41 +1,43 @@
-const Discord = require('discord.js'); // creates an instance of discord.js
-//require('dotenv-flow').config(); // creates an instance of dotenv-flow for token use
-const client = new Discord.Client();
-const { prefix } = require("./config.json");
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
-const token = "Njk4MTI0NDEwOTUwOTA5OTgz.XpBXnA.qJJTXMXhdyofyeMK45MU_8XKW5U";
+const token = `ENTER TOKEN HERE`;
 
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-})
-
-client.on('message', (receivedMessage)=>{
-  if (msg.content === `nm reminder`) {
-      msg.channel.send('Please enter reminder details in the following\n'
-      + 'format:\n'
-      + 'set reminder,dd/mm/yyyy,hh:mm:ss,details')
-
-    }
+bot.once('ready', () =>{
+  console.log('Bot online');
 });
 
-//Splits all information for retrieval later
-let date = ''
-let time = ''
-let details = ''
-client.on('message', (receivedMessage)=>{
+let date = '';
+let time = '';
+let details = '';
 
-if (msg.content.startsWith('set reminder')) {
-    let reminderParts = msg.content.split(',', 4)
-    date = reminderParts[1]
-    time = reminderParts[2]
-   details = reminderParts[3]
-   msg.channel.send('reminder set');
+bot.on('message', msg=>{
+function checkTime(){
+let d = new Date();
+let lDate = d.toLocaleDateString();
+let lTime = d.toLocaleTimeString();
+if (lDate === date && lTime === time){
+msg.channel.send(details); }
  }
+
+
+if (msg.content === 'nm reminder'){
+  msg.channel.send('Please enter reminder details in the following\n' + 'format:\n'
++ 'set reminder,dd/mm/yyyy,hh:mm:ss am/pm,details\n'
++ 'No 0 in time for single hour time')
+}
+
+
+if (msg.content.startsWith('set reminder')){
+  let reminderParts = msg.content.split(',', 4);
+  date = reminderParts[1];
+  time = reminderParts[2];
+  details = reminderParts[3];
+  setInterval(checkTime, 1000);
+  msg.channel.send('reminder set');
+}
+
 });
-setInterval((time,date,details)=>{
-    let d = new Date();
-    if (d.toLocaleDateString() === date && d.toLocaleTimeString() === time) {
-      msg.channel.send(details)
-    }
-  }, 1000);
-//client.login("Njk4MTI0NDEwOTUwOTA5OTgz.XpBXnA.qJJTXMXhdyofyeMK45MU_8XKW5U");
+
+
+bot.login("Njk4MTI0NDEwOTUwOTA5OTgz.XrkAnQ.Gwn_4y1xVQKzjW_z2ap40UEHW0g");
