7cabc4467602c68b2ca5966e4f7b59eea72128e8
Ryan Cetinski <ryancetinskii@gmail.com>
2020-05-17 17:16:10 +1000

    Complete google search functionality

--- bot.js
+++ bot.js
@@ -3,35 +3,61 @@
 const Discord = require('discord.js'); // creates an instance of discord.js
 
 // jQuery is used to handle the search data, and jsdom makes jquery usable outside of a web browser.
-const { JSDOM } = require( 'jsdom' )
+const { JSDOM } = require('jsdom')
 const { window } = new JSDOM("")
-const $ = require( 'jquery' )( window )
-const SEARCH_KEY = process.env.SERP_API_KEY
+const $ = require('jquery')(window)
+//const SEARCH_KEY = process.env.SERP_API_KEY
 
 require('dotenv-flow').config(); // creates an instance of dotenv-flow for token use
 const client = new Discord.Client();
 const { prefix } = require("./config.json");
 
 client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 // Event listener when a user sends a message in the chat
 client.on('message', msg => {
 
+  if (msg.author.bot) return // ignores messages from other bots
+
   // The command 'nm status' causes the bot to say 'Online!'
   if (msg.content === `${prefix}status`) {
     msg.channel.send('Online! :robot:');
   }
- 
-// The command 'nm calc' followed by a calculation returns an answer
-// to the user.
+
+  // The command 'nm calc' followed by a calculation returns an answer
+  // to the user.
   if (msg.content.startsWith(`${prefix}calc`)) {
-      let parts = msg.content.split(' ', 3)
-      let problem = parts[2]
-      let answer = eval(problem)
-      msg.channel.send(answer);
-    }
+    let parts = msg.content.split(' ', 3)
+    let problem = parts[2]
+    let answer = eval(problem)
+    msg.channel.send(answer);
+  }
+
+  // Web Search
+  if (msg.content.startsWith(`${prefix}search`)) {
+    let query = msg.content.slice(10, msg.content.length)
+    console.log(query)
+
+    let url = `http://api.serpstack.com/search?access_key=${process.env.SERP_API_KEY}&type=web&query=${query}`
+    console.log(url)
+
+    let output = ">>> Here are your results:\n\n"
+
+    $.get(url, function (data) {
+
+      // prints some info about the request to the API
+      console.log(data.request)
+
+      for (i = 0; i < 5; i++) {
+        output += (i + 1) + ". " + data.organic_results[i].title + "\n" + data.organic_results[i].url + "\n\n"
+      }
+
+      msg.channel.send(output)
+
+    })
+  }
 });
 
 
