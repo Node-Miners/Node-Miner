bae7a3d1c71bf5d37402a74564f7eeff2d21d069
court1985 <48073202+court1985@users.noreply.github.com>
2020-05-20 19:02:23 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,6 +25,36 @@ client.on('message', msg => {
       let answer = eval(problem)
       msg.channel.send(answer);
     }
+    
+    let date = '';
+let time = '';
+let details = '';
+
+bot.on('message', msg=>{
+function checkTime(){
+let d = new Date();
+let lDate = d.toLocaleDateString();
+let lTime = d.toLocaleTimeString();
+if (lDate === date && lTime === time){
+msg.channel.send(details); }
+ }
+
+
+if (msg.content === 'nm reminder'){
+  msg.channel.send('Please enter reminder details in the following\n' + 'format:\n'
++ 'set reminder,dd/mm/yyyy,hh:mm:ss am/pm,details\n'
++ 'No 0 in time for single hour time')
+}
+
+
+if (msg.content.startsWith('set reminder')){
+  let reminderParts = msg.content.split(',', 4);
+  date = reminderParts[1];
+  time = reminderParts[2];
+  details = reminderParts[3];
+  setInterval(checkTime, 1000);
+  msg.channel.send('reminder set');
+}
 });
 
 
